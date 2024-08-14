490ac3e332e41538470daf7f1891650376d861da
Francisco Noé Arriaga <112121312+FNoeCoder@users.noreply.github.com>
2024-08-13 22:40:03 -0600

    Agregar extracción de comentarios y calificación del producto

--- ScrapingML.py
+++ ScrapingML.py
@@ -47,19 +47,18 @@ class ScrapingML:
             self.tituloBusqueda = titulo_input["value"] if titulo_input else "No disponible"
 
             for product in products:
-                nombre_tag = product.find("h2", class_="ui-search-item__title")
-                nombre = nombre_tag.text.strip() if nombre_tag else "No disponible"
-                
-                precio_tag = product.find("span", class_="andes-money-amount__fraction")
-                precio = precio_tag.text.strip() if precio_tag else "No disponible"
-                
                 enlace_tag = product.find("a", class_="ui-search-item__group__element ui-search-link__title-card ui-search-link")
                 enlace = enlace_tag["href"] if enlace_tag else "No disponible"
-                
+
+                producto = ScrapingProductoML(enlace)
+                nombre = producto.getNombre()
+                precio = producto.getPrecio()
+                descripcion = producto.getDescripcion()
+                comentarios = producto.getComentarios()
+                calificacion = producto.getCalificacion()
                 imagen_tag = product.find("img", class_="ui-search-result-image__element")
                 imagenEnlace = imagen_tag["data-src"] if imagen_tag else "No disponible"
-                
-                descripcion = ScrapingProductoML(enlace).getProducto()["descripcion"] if enlace != "No disponible" else "No disponible"
+            
 
                 if precio != "No disponible":
                     self.precios.append(float(precio.replace(",", "")))
@@ -68,144 +67,25 @@ class ScrapingML:
                     "Nombre": nombre,
                     "Precio": precio,
                     "Enlace": enlace,
-                    "Imagen": imagenEnlace, 
-                    "Descripcion": descripcion
+                    "ImagenPortada": imagenEnlace, 
+                    "Descripcion": descripcion,
+                    "Comentarios": comentarios,
+                    "Calificacion": calificacion,
                 })
-            self.calcularPrecioPromedio()
-            self.calcularMediana()
-            self.calcularDesviacionEstandar()
-            return True
-        else:
-            return False
-    def calcularPrecioPromedio(self):
-        if len(self.precios) > 0:
-            self.precioPromedio = sum(self.precios) / len(self.precios)
-            #redondear a 2 decimales
-            self.precioPromedio = round(self.precioPromedio, 2)
-            return True
-        else:
-            return False
-    def calcularMediana(self):
-        if len(self.precios) > 0:
-            self.mediana = self.precios[len(self.precios) // 2]
-            return True
-        else:
-            return False
-    def calcularDesviacionEstandar(self):
-        if len(self.precios) > 0:
-            self.desviacionEstandar = (sum([(x - self.precioPromedio) ** 2 for x in self.precios]) / len(self.precios)) ** 0.5
             return True
         else:
             return False
 
-    def guardarDatosExcel(self):
-        if self.conexionEstablecida:
-            df = pd.DataFrame(self.data)  # Crear DataFrame a partir de la lista de diccionarios
-            # Crear un DataFrame con el precio promedio
-            df_precio_promedio = pd.DataFrame([{
-                "Nombre": "Precio promedio",
-                "Precio": self.precioPromedio,
-                "Enlace": "",
-                "Imagen": "",
-                "Descripcion": ""
-            }])
-            df_precio_mediana = pd.DataFrame([{
-                "Nombre": "Mediana",
-                "Precio": self.mediana,
-                "Enlace": "",
-                "Imagen": "",
-                "Descripcion": ""
-            }])
-            df_desviacion_estandar = pd.DataFrame([{
-                "Nombre": "Desviación estándar",
-                "Precio": self.desviacionEstandar,
-                "Enlace": "",
-                "Imagen": "",
-                "Descripcion": ""
-            }])
-            # Concatenar el DataFrame original con el del precio promedio
-            df = pd.concat([df, df_precio_promedio, df_precio_mediana, df_desviacion_estandar], ignore_index=True)
-            df.to_excel(f"./Archivos_Generados/Excel/{self.tituloBusqueda.replace(" ", "_")}.xlsx", index=False)
-            return True
-        else:
-            return False
     def guardarDatosJSON(self):
         if self.conexionEstablecida:
             with open(f"./Archivos_Generados/JSON/{self.tituloBusqueda.replace(" ", "_")}.json", "w", encoding="utf-8") as file:
-                json.dump({
-                    "Titulo de la busqueda": self.tituloBusqueda,
-                    "Datos": self.data,
-                    "Precio promedio": self.precioPromedio,
-                    "Mediana": self.mediana,
-                    "desviacion estandar": self.desviacionEstandar
-                }, file, ensure_ascii=False, indent=4)
+                json.dump(self.data, file, ensure_ascii=False, indent=4)
             return True
         else:
             return False
-    def graficarPrecios(self, tipo = "cercanos"):
-        if len(self.precios) > 0:
-            self.productosSeleccionados = self.data
-            if "baratos" == tipo:
-                self.productosSeleccionados = self.getDatosMasBarato()
-            elif "caros" == tipo:
-                self.productosSeleccionados = self.getDatosMasCaro()
-            elif "cercanos" == tipo:
-                self.productosSeleccionados = self.getDatosMasCercaPromedio()
-            elif "todos" == tipo:
-                self.productosSeleccionados = self.data
-            else:
-                return False
 
-            plt.figure(figsize=(20, 10))
-            plt.bar([producto["Nombre"][:15] for producto in self.productosSeleccionados], [float(producto["Precio"].replace(",", "")) for producto in self.productosSeleccionados], color="skyblue", label="Precio")
-            plt.axhline(y=self.precioPromedio, color='r', linestyle='-', label=f"Precio promedio: ${self.precioPromedio}")
-            plt.axhline(y=self.mediana, color='g', linestyle='-', label=f"Mediana: ${self.mediana}")
-            plt.axhline(y=self.desviacionEstandar, color='y', linestyle='--', label=f"Desviación estándar: ${self.desviacionEstandar}")
-            plt.xticks(rotation=90)
-            plt.xlabel("Productos")
-            plt.ylabel("Precio")
-            plt.title(f"Precios de {self.tituloBusqueda} (más {tipo})")
-            plt.xticks(fontsize=20)
-            plt.legend()
-            plt.tight_layout()
-            plt.savefig(f"./Archivos_Generados/IMG/{self.tituloBusqueda.replace(' ', '_')}_{tipo}.png")
-            plt.close()
-            del self.productosSeleccionados
-            return True
-        else:
-            return False
     def datosEncontrados(self):
         return len(self.data) > 0
-    def getDatosMasBarato(self):
-        if len(self.data) > 0:
-            # Obtener los 10 productos más baratos
-            return sorted(self.data, key=lambda x: float(x["Precio"].replace(",", "")))[:10]
-        else:
-            return None
-    def getDatosMasCaro(self):
-        if len(self.data) > 0:
-            # Obtener los 10 productos más caros
-            return sorted(self.data, key=lambda x: float(x["Precio"].replace(",", "")), reverse=True)[:10]
-        else:
-            return None
-    def getDatosMasCercaPromedio(self):
-        if len(self.data) > 0:
-            # Obtener los 10 productos más cercanos al precio promedio
-            return sorted(self.data, key=lambda x: abs(float(x["Precio"].replace(",", "")) - self.precioPromedio))[:10]
-        else:
-            return None
-
-# # url directamente de la página
-# url = "https://listado.mercadolibre.com.mx/laptop-gamer#D[A:laptop%20gamer]"
-# scraping = ScrapingML(url)
 
 
-# if scraping.establecerConexion() and scraping.extraerDatos():
-#     print("Datos extraídos correctamente")
-#     scraping.guardarDatosExcel()
-#     scraping.guardarDatosJSON()
-#     scraping.graficarPrecios(tipo="todos")
-#     print("Datos guardados correctamente")
-# else:
-#     print("Error al establecer la conexión o extraer datos")
 
--- ScrapingProductoML.py
+++ ScrapingProductoML.py
@@ -16,14 +16,34 @@ class ScrapingProductoML:
         nombre_tag = self.soup.find('h1', class_='ui-pdp-title')
         precio_tag = self.soup.find('span', class_='andes-money-amount__fraction')
         descripcion_tag = self.soup.find('p', class_='ui-pdp-description__content')
+        comentarios_tag = self.soup.find('div', class_='ui-review-capability-comments')
+        calificacion_tag = self.soup.find('span', class_='ui-pdp-review__rating')
+        comentarios = []
+        for comentario in comentarios_tag.find_all('article', class_='ui-review-capability-comments__comment'):
+            # la etiqueta p contiene el texto del comentario y tiene la clase ui-review-capability-comments__comment__content ui-review-capability-comments__comment__content
+            comentarios.append( comentario.find('p', class_='ui-review-capability-comments__comment__content').text if comentario.find('p', class_='ui-review-capability-comments__comment__content') else "No disponible" )
         
         self.producto['nombre'] = nombre_tag.text if nombre_tag else "No disponible"
         self.producto['precio'] = precio_tag.text if precio_tag else "No disponible"
         self.producto['descripcion'] = descripcion_tag.text if descripcion_tag else "No disponible"
+        self.producto['comentarios'] = comentarios
+        self.producto['calificacion'] = calificacion_tag.text if calificacion_tag else "No disponible"
+
 
     def getProducto(self):
         return self.producto
+    def getNombre(self):
+        return self.producto['nombre']
+    def getPrecio(self):
+        return self.producto['precio']
+    def getDescripcion(self):
+        return self.producto['descripcion']
+    def getComentarios(self):
+        return self.producto['comentarios']
+    def getCalificacion(self):
+        return self.producto['calificacion']
 
-# url = "https://www.mercadolibre.com.mx/laptop-gamer-thunderobot-911mt-12th-intel-core-i7-12650h-16gb-de-ram-512gb-ssd-nvidia-geforce-rtx-3050-165-hz-1920x1080px-windows-11-pro/p/MLM28131164#searchVariation%3DMLM28131164%26position%3D4%26search_layout%3Dstack%26type%3Dproduct%26tracking_id%3Dbb395668-a8e7-455b-a09f-f11edff31912"
-# producto = ScrapingProductoML(url).getProducto()
-# print(producto["descripcion"])
+if __name__ == "__main__":
+    url = "https://www.mercadolibre.com.mx/laptop-gamer-thunderobot-911mt-12th-intel-core-i7-12650h-16gb-de-ram-512gb-ssd-nvidia-geforce-rtx-3050-165-hz-1920x1080px-windows-11-pro/p/MLM28131164#searchVariation%3DMLM28131164%26position%3D4%26search_layout%3Dstack%26type%3Dproduct%26tracking_id%3Dbb395668-a8e7-455b-a09f-f11edff31912"
+    producto = ScrapingProductoML(url).getProducto()
+    print(producto)
